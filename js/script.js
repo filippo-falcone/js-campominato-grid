e54d313213052e7766ac493b7894b3f2f3308a44
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-16 09:35:41 +0100

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@
    - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 ------------------------------------------------------------------------------------------------------------------------------------------------------- */
 /*
-/*
 // Metto in allerta il bottone
 // Per 100 volte genero un quadrato
 // Lo aggiungo nel DOM
@@ -21,15 +20,15 @@
 const playButton = document.querySelector('#play-btn');
 const mainContainer = document.querySelector('.ms-container');
 const mainGrid = document.querySelector('#ms-grid');
-let numberSquare = 100
 /* EVENTS */
 playButton.addEventListener('click', function () {
     const footer = document.querySelector('footer');
+    const difficultly = changeDifficulty('easy', 'medium', 'hard', 100, 81, 49);
     footer.classList.remove('d-none');
     footer.classList.add('d-flex');
     mainContainer.classList.remove('d-none');
     mainGrid.innerHTML = '';
-    for (let i = 1; i <= numberSquare; i++) {
+    for (let i = 1; i <= difficultly; i++) {
         const square = createSquare(i);
         mainGrid.append(square);
         square.addEventListener('click', function () {
@@ -45,12 +44,36 @@ playButton.addEventListener('click', function () {
 // number: dato di tipo numerico
 // return: elemento nel DOM rappresentante un quadrato
 function createSquare(number) {
+    const difficultySelect = document.querySelector('#difficulty').value;
     const square = document.createElement('div');
+    if (difficultySelect === 'hard') {
+        square.classList.add('ms-square-hard');
+
+    } else if (difficultySelect === 'medium') {
+        square.classList.add('ms-square-medium');
+    } else {
+        square.classList.add('ms-square-easy');
+    }
     square.classList.add('ms-square');
-    square.classList.add('ms-square-easy');
     square.classList.add('d-flex');
     square.classList.add('justify-content-center');
     square.classList.add('align-items-center');
     square.innerHTML = `<span>${number}</span>`;
     return square;
+}
+// Funzione per cambiare livello di difficoltà
+// value1, value2, value3: elemento di tipo stringa che rappresenta il value nel DOM
+// number1, number2, number3: elemento di tipo numero
+// return: variabile che cambia la diffcoltà
+function changeDifficulty(value1, value2, value3, number1, number2, number3) {
+    let difficulty;
+    const difficultySelect = document.querySelector('#difficulty').value;
+    if (difficultySelect === value1) {
+        difficulty = number1;
+    } else if (difficultySelect === value2) {
+        difficulty = number2;
+    } else if (difficultySelect === value3) {
+        difficulty = number3;
+    }
+    return difficulty;
 }
